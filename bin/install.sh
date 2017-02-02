c8605fc20c6b2dbce0111d7a8977a24ac004ba78
Jan Hybš <jan.hybs@tul.cz>
2017-02-02 18:15:07 +0100

    Add mpich with blcr version

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,7 @@
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CWD"
 
-docker build --tag base    ../base
-docker build --tag openmpi ../openmpi
-docker build --tag mpich   ../mpich
+docker build --tag base         ../base
+docker build --tag openmpi      ../openmpi
+docker build --tag mpich        ../mpich
+docker build --tag mpich-blcr   ../mpich-blcr
